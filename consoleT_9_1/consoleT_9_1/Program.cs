af33e7efe9d83189735a299acd110c1adf5afd00
MorozovIvan320 <96531862+MorozovIvan320@users.noreply.github.com>
2021-12-22 17:11:39 +0300

    Add files via upload

---
+++ consoleT_9_1/consoleT_9_1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleT_9_1
+{
+    class Task_1
+    {
+        string[] str;
+        string result = "";
+        string path = AppDomain.CurrentDomain.BaseDirectory + "\\task1.bin";
+        public Task_1(string str, char a)
+        {
+            this.str = str.Split(' ');
+            result = f(a);
+            if (result == "") 
+            {
+                result = "Нет слов на введенную букву";
+            }
+            Write();
+        }
+        string f(char a)
+        {
+            StringBuilder sBuild = new StringBuilder(String.Empty);
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.Parse(str[i][0].ToString().ToLower()) == a)
+                {
+                  sBuild.Append(str[i] + " ");
+                }
+            }
+            return sBuild.ToString();
+        }
+        void Write()
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
+            {
+                writer.Write(result);
+                writer.Close();
+            }
+        }
+        public string Read()
+        {
+            using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+            {
+                string str = reader.ReadString();
+                reader.Close();
+                return str;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите слова через пробел: ");
+                string str = Console.ReadLine();
+                Console.Write("Введите символ: ");
+                char a = Convert.ToChar(Console.ReadLine());
+                Task_1 o = new Task_1(str, a);
+                Console.WriteLine("Результат:\n{0}", o.Read());
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+        }
+    }
+}
---
+++ consoleT_9_2/consoleT_9_2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleT_9_2
+{
+    class Task2
+    {
+        string path = AppDomain.CurrentDomain.BaseDirectory + "\\file.txt";
+        string wPath = AppDomain.CurrentDomain.BaseDirectory + "\\File2.txt";
+        string[] strArr;
+        public Task2()
+        {
+            strArr = File.ReadAllLines(path, Encoding.Default);
+        }
+        public void F()
+        {
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                strArr[i] += $" ({strArr[i].Length})";
+            }
+            Write();
+        }
+        void Write()
+        {
+            File.WriteAllLines(wPath, strArr);
+        }
+        public string Str
+        {
+            get
+            {
+                return String.Join('\n'.ToString(), strArr);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Task2 o = new Task2();
+                Console.WriteLine($"Текст:\n{o.Str}");
+                o.F();
+                Console.WriteLine($"Измененный текст:\n{o.Str}");
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+        }
+    }
+}
